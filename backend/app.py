c86f6fc2830b12de0438da8f33181bed19336241
Uponnmanmi Pontip <uponnmanmipontipnimlyat@gmail.com>
2026-05-16 17:57:51 +0100

    made post id and subppaths

--- backend/app.py
+++ backend/app.py
@@ -18,11 +18,23 @@ def greet_user(username):
         "status": "success"
     })
 
+@app.route('/hello')
+def hello():
+    name = request.args.get("name", "flask")
+    return f"<h1>Hello World, Welcome to my flask App, my name is, {escape(name)}!</h1>"
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
 
-@app.route("/hi")
-def hi():
-    name = request.args.get("name", "Flask")
-    return f"Hi, {escape(name)}!"
+@app.route('/post/<int:post_id>/')   
+def show_post(post_id):
+    return f'post_id {escape(post_id)}'
+
+@app.route('/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {escape(subpath)}'
+
 if __name__ == '__main__':
     app.run(debug=True)
