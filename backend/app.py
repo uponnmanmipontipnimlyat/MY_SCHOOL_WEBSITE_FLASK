a9bbba242efdec2e6a5739f62767b162afc84b80
Uponnmanmi Pontip <uponnmanmipontipnimlyat@gmail.com>
2026-05-27 09:29:54 +0100

    created a requirements.txt file

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, url_for
 from flask import request
 from markupsafe import escape
 app  = Flask(__name__)
@@ -6,17 +6,7 @@ app  = Flask(__name__)
 def home():
     return "Hello, World!"
 
-@app.route('/greetings')
-def greetings():
-    return '<h1>Welcome to the Flask App!</h1>'
 
-@app.route('/greet-user/<username>')
-def greet_user(username):
-    """Return a JSON greeting for the provided username."""
-    return jsonify({
-        "message": f"Hello, {username}!",
-        "status": "success"
-    })
 
 @app.route('/hello')
 def hello():
@@ -29,12 +19,13 @@ def show_user_profile(username):
     return f'User {escape(username)}'
 
 @app.route('/post/<int:post_id>/')   
-def show_post(post_id):
+def show_post(post_id):                      
     return f'post_id {escape(post_id)}'
 
 @app.route('/<path:subpath>')
 def show_subpath(subpath):
     return f'Subpath {escape(subpath)}'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
